f55a0b41b04a3007b304e11a2a7d608f99a63047
mingun choi <ryanchoi100826@gmail.com>
2023-02-01 15:51:55 -0500

    modified newTweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $( document ).ready(function() {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
   
 const renderTweets = function(tweets) {
   const $container = $('#tweets-container')
@@ -46,18 +70,8 @@ const createTweetElement = function(tweet) {
 
     return $tweet;
   }
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
- }
 
+renderTweets(data);
 const $tweet = createTweetElement(tweetData);
 
 // Test / driver code (temporary)
