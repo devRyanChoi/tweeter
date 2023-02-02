2a196af6c1177d5f54dea3b1b0c143997f901193
mingun choi <ryanchoi100826@gmail.com>
2023-02-01 20:48:43 -0500

    clients modified

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,64 +4,33 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $( document ).ready(function() {
+  console.log( "ready!" );
 
-  $( "form" ).on( "submit", function( event ) {
+  $('form').on('submit',function(event) {
     event.preventDefault();
-    console.log( $( this ).serialize() );
-  });
-
+  });  
+  const tweetText = $(this).serialize();
   
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-  
-const renderTweets = function(tweets) {
-  const $container = $('#tweets-container')
+  const renderTweets = function(tweets) {
+    const $container = $('#tweets-container')
 
-  tweets.forEach(tweet => {
-      const $tweet = createTweetElement(tweet);
-      $container.prepend($tweet);
-  });
-}
-
-const createTweetElement = function(tweet) {
-  const escape = function (str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
+    tweets.forEach(tweet => {
+        const $tweet = createTweetElement(tweet);
+        $container.prepend($tweet);
+    });
   };
-  
+
+
+  const createTweetElement = function (data) {
   const $tweet = `
-  <article>
-      <div class="tweetpost">
-        <ul>
-         <img src="${data.user.avatars}">
-         <h4>${data.user.name}</h4>
-        </ul> 
-        <h5>${data.user.handle}</h5>
-      </div>
-      
+    <article>
+        <div class="tweetpost">
+          <ul>
+           <img src="${data.user.avatars}">
+           <h4>${data.user.name}</h4>
+          </ul> 
+          <h5>${data.user.handle}</h5>
+        </div>
         <div class="postedtweet">
         <p class="tweet-text">${escape(data.content.text)}</p>
         </div>
@@ -74,18 +43,14 @@ const createTweetElement = function(tweet) {
           </div>
         </div>  
     </article> `
+        
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
     return $tweet;
   }
 
-renderTweets(data);
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet);
 });
-
-
-
-
