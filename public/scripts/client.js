4538468c2b3554b9e2ccf35c50f1ff11f6e427ea
mingun choi <ryanchoi100826@gmail.com>
2023-02-01 21:54:04 -0500

    clinet modified

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,67 +7,88 @@ $( document ).ready(function() {
   console.log( "ready!" );
 
   $('form').on('submit',function(event) {
-    event.preventDefault();
-    
+  event.preventDefault();
+
+  const newTweetText = $('#tweet-text').val();
   const tweetText = $(this).serialize();
-  });
-  
-  function loadTweets() {
-    $.get("/tweets")
-    .then(data => {
-    renderTweets(data)
-    })
-    $.ajax('/tweets', { method: 'GET' })
-    .then(function (moreTweets) {
-      renderTweets(moreTweets);
-    })
-    .catch((err) => {
-      console.log(err);
+
+    if (newTweetText.length > 0 && newTweetText.length <= 140) {
+      $.post("/tweets", tweetText)
+        .done(function() {
+          // Reset form and counter on successful post
+          $('#tweet-text').val('');
+          $('.counter').val(140);
+
+          // Clear any error messages
+          $('#error-container')
+            .removeClass('unhide')
+            .slideUp('slow');
+
+          // Refresh the tweet container
+          $('#tweets-container').empty();
+          loadTweets();
+        });
+      }
     });
-  }
+
+    function loadTweets() {
+
+      $.get("/tweets")
+        .then(data => {
+          renderTweets(data)
+        })
+      $.ajax('/tweets', { method: 'GET' })
+      .then(function (moreTweets) {
+        renderTweets(moreTweets);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }
+  
     loadTweets();
 
-  const renderTweets = function(tweets) {
-    
-    tweets.forEach(tweet => {
+const renderTweets = function(tweets) {
+  const $container = $('#tweets-container')
+
+  tweets.forEach(tweet => {
       const $tweet = createTweetElement(tweet);
       $container.prepend($tweet);
-    });
-    const $container = $('#tweets-container') 
-    
-  };
+  });
+};
 
 
-  const createTweetElement = function (data) {
+const createTweetElement = function (data) {
 
-    const escape = function (str) {
-        let div = document.createElement("div");
-        div.appendChild(document.createTextNode(str));
-        return div.innerHTML;
-      };
-    const $tweet = `
-    <article>
-        <div class="tweetpost">
-          <ul>
-           <img src="${data.user.avatars}">
-           <h4>${data.user.name}</h4>
-          </ul> 
-          <h5>${data.user.handle}</h5>
+  const escape = function (str) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    };
+  const $tweet = `
+  <article>
+      <div class="tweetpost">
+        <ul>
+         <img src="${data.user.avatars}">
+         <h4>${data.user.name}</h4>
+        </ul> 
+        <h5>${data.user.handle}</h5>
+      </div>
+      
+        <div class="postedtweet">
+        <p class="tweet-text">${escape(data.content.text)}</p>
         </div>
-        
-          <div class="postedtweet">
-          <p class="tweet-text">${escape(data.content.text)}</p>
+        <div class="timeicon">
+        <p class="time">${timeago.format(data.created_at)}</p>
+          <div class="icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i> 
+          <i class="fa-solid fa-heart"></i>
           </div>
-          <div class="timeicon">
-          <p class="time">${timeago.format(data.created_at)}</p>
-            <div class="icons">
-            <i class="fa-solid fa-flag"></i>
-            <i class="fa-solid fa-retweet"></i> 
-            <i class="fa-solid fa-heart"></i>
-            </div>
-          </div>  
-      </article> `
+        </div>  
+    </article> `
 
-      return $tweet;
+    return $tweet;
+}
 
 });
